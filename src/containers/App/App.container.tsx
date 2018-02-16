9f9faabc84f23c2c8ba0f5decc1e2aec842760e7
Jonjoe Whitfield <me@jonjoe.io>
2018-02-16 13:03:46 +0000

    project setup

---
+++ src/containers/App/App.container.tsx
@@ -0,0 +1,47 @@
+/*
+  | App Component ============================================
+  ------------------------------------------------------------------
+  | Component for rendering a list of notes.
+*/
+
+// Imports =========================================================
+// -----------------------------------------------------------------
+// Base Imports
+import * as React from 'react';
+
+// Module Imports
+// ...
+
+// Component Imports
+// ...
+
+// Interfaces ======================================================
+// -----------------------------------------------------------------
+interface State {}
+interface Props {}
+
+// React Component Class ===========================================
+// -----------------------------------------------------------------
+class App extends React.Component<Props, State> {
+  // Class constructor
+  constructor(props: Props) {
+    super(props);
+  }
+
+  // Event handlers
+  // ...
+
+  // Custom Methods
+  // ...
+
+  // React API hooks
+  componentDidMount() {}
+
+  render(): JSX.Element {
+    return <p>cswdewrf</p>;
+  }
+}
+
+// Exports =========================================================
+// -----------------------------------------------------------------
+export default App;
